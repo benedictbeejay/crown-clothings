efbc57a6a70fa0f8c7190641c8f4b66cf7408dfd
benedictbeejay <benedictbeejay@gmail.com>
2024-01-29 02:52:46 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './routes/home/home.component';
 import {  Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
-
+import SignIn from './routes/sign-in/sign-in.component';
 const Shop = () => {
   return (
     <h1>i am the shop page</h1>
@@ -15,9 +15,10 @@ const App = () => {
         <Route path='/' element={<Navigation />}>
           <Route index element={<Home />} />
           <Route path="shop" element={<Shop />} />
+          <Route path="sign-in" element={<SignIn />} />
         </Route>
       </Routes>
   );
 };   
-
+ 
 export default App;
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+        <h1>
+            sign in page
+        </h1>
+    </div>
+  )
+}
+
+export default SignIn

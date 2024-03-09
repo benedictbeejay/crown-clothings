dd9352c300f4fda617e9812cb35e9f2826e0a85a
benedictbeejay <benedictbeejay@gmail.com>
2024-03-09 16:36:36 +0100

    updated files

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import Home from './routes/home/home.component';
 import {  Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication   from './routes/authentication/authentication.component';
-const Shop = () => {
-  return (
-    <h1>i am the shop page</h1>
-  );
-}
-
+import Shop from './routes/shop/shop.component';
 const App = () => {
   return (
       <Routes>

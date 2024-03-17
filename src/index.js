bdad4c4acd8cc83ef0eecd18f05a5d220fa40288
benedictbeejay <benedictbeejay@gmail.com>
2024-03-17 22:35:54 +0100

    updated files

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import App from './App';
 import { UserProvider } from './context/user.context';
 import { ProductsProvider } from './context/products.context'
 import { BrowserRouter } from 'react-router-dom';
-
+import { CartProvider } from './context/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
-          <App />
-        </ProductsProvider>  
+          <ProductsProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </ProductsProvider>  
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Fragment, useContext } from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import { UserContext } from '../../context/user.context'
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import './navigation.styles.scss'
-import { UserContext } from '../../context/user.context'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
+
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
+import { CartContext } from '../../context/cart.context'
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  
+  const { isCartOpen } = useContext(CartContext)
   
   return (
     <Fragment>
@@ -30,8 +34,9 @@ const Navigation = () => {
                      </Link>
                   )
                 }
-                
-            </div>         
+               <CartIcon /> 
+            </div> 
+            {isCartOpen && <CartDropdown />}       
         </div>
         <Outlet />
     </Fragment>

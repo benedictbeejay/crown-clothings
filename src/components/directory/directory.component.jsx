45a2891961c7aa2cc4b599d3fd833e3d73bfd269
benedictbeejay <benedictbeejay@gmail.com>
2024-04-02 12:43:38 +0100

    updated files

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './directory.styles.scss'
-import CategoryItem from '../category-item/category-item.component';
+// import CategoryItem from '../directory-item/category-item.component';
 
+import DirectoryItem from '../directory-item/directory-item.component'
 const Directory = ({categories}) => {
   return (
      <div className="categories-container">
       {categories.map((category) => (
-        <CategoryItem  key={category.id} category={category} />
+        <DirectoryItem  key={category.id} category={category} />
       ))}
      
     </div>

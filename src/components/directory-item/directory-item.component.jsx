ef836cef7b626c47a06876a533604a8fa7513cab
benedictbeejay <benedictbeejay@gmail.com>
2024-04-08 11:05:45 +0100

    updated files

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,24 @@
-import './directory-item.styles.scss'
-import React from 'react';
+import { useNavigate } from 'react-router-dom'
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
-  return (
-     <div className="directory-item-container">
-        <div className='background-image' style={{
-          backgroundImage: `url(${imageUrl})`
-        }} />
-        <div className="body">
-          <h2>{title}</h2>
-          <p>Shop Now</p> 
-        </div>
-      </div>  
-  )
-}
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
 
-export default  DirectoryItem
- 
+  const onNavigateHandler = () => navigate(route);
+  return (
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </Body>
+    </DirectoryItemContainer>
+  );
+};
+
+export default DirectoryItem;
--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import './form-input.styles.scss'
-const FormInput = ({ label, ...otherProps}) => {
+
+import { FormInputLabel, Input, Group } from './form-input.styles';
+
+const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-         <input className='form-input'
-        {...otherProps}
-      />
-    {label && (
+    <Group>
+      <Input {...otherProps} />
+      {label && (
+        <FormInputLabel shrink={otherProps.value.length}>
+          {label}
+        </FormInputLabel>
+      )}
+    </Group>
+  );
+};
 
-        <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
-            {label}
-        </label>
-    )}          
-     
-    </div>
-  )
-}
-
-export default FormInput
+export default FormInput;
